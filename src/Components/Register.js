5611869c020eb914d0314a4ef568ece84c5a6c07
Dinesh yadav <dineshyadav74950@gmail.com>
2024-04-17 00:38:02 +0530

    implementing media queries and hover in react native

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,7 +1,8 @@
 import React from "react";
-
+import Styled from "styled-components";
 export default  function Register(props) {
   
+
     const btnStyle ={
         backgroundColor:"green",
         color:"white"
@@ -18,8 +19,31 @@ export default  function Register(props) {
         passBoxType = "password";  
         btnClasses.push("btn-success");     
     }
-   
-        return <div className="container card p-3 mt-2 register-container App-header">
+    const StyledButton = Styled.button`
+    display:block;
+    padding:10px 5px;
+    background-color:orange;
+    border:none;
+    color:white;
+    width:100%;
+    margin:5px ;
+    `;
+    const StyledDiv = Styled.div`
+    background:purple;
+    height:200px;
+    width:400px;
+    `;
+    const StyledRegisterContainer = Styled.div`
+    width:550px;
+    &:hover{
+        box-shadow:0px 0px 70px purple;
+    }
+    @media (min-width:0px) and (max-width:550px){
+        width:300px;
+    }
+    `;
+    // register-container
+        return <StyledRegisterContainer className="container card p-3 mt-2  App-header">
             <form className="form-group" onSubmit={props.submit}>
              <h1 className="text-center">Registration Form</h1>
                 <div>
@@ -37,6 +61,11 @@ export default  function Register(props) {
                 <br/>
             <button type="submit" className="btn btn-primary">Register</button> 
             <button type="text" className={btnClasses.join(" ")} onClick={props.click} style={btnStyle}> {btnText}</button>
+            <StyledButton type="button">Login</StyledButton>
+            <StyledButton type="button">T & C</StyledButton>
+            <StyledDiv>
+                <h3>This inbuild styled div</h3>
+            </StyledDiv>
             </form>
-        </div>
+        </StyledRegisterContainer>
 }
